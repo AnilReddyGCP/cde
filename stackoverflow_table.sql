d63fa6a4c4893898e2b0b1eb0df7a7acbab44864
anilreddy <chanilreddy1228@gmail.com>
2023-06-29 17:43:38 +0530

    stackoverflow table with out partition

---
+++ stackoverflow_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE
+ stack_oveflow_dataset.stackoverflow_table1( id INTEGER,
+    title STRING,
+    body STRING,
+    accepted_answer_id INTEGER,
+    answer_count INTEGER,
+    comment_count INTEGER,
+    community_owned_date TIMESTAMP,
+    creation_date TIMESTAMP,
+    favorite_count INTEGER,
+    last_activity_date TIMESTAMP,
+    last_edit_date TIMESTAMP,
+    last_editor_display_name STRING,
+    last_editor_user_id INTEGER,
+    owner_display_name STRING,
+    owner_user_id INTEGER,
+    parent_id INTEGER,
+    post_type_id INTEGER,
+    score INTEGER,
+    tags STRING,
+    view_count INTEGER );
+
+insert into stack_oveflow_dataset.stackoverflow_table1 select * FROM `bigquery-public-data.stackoverflow.stackoverflow_posts`
